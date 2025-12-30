6fddbaad77be37bc73daaa297702be498b5b1e0e
assiaab052-arch <assiaab052@gmail.com>
2025-12-30 20:24:45 +0100

    Ajout TodoListOfflineScreen et ThemeContext, modifications App.js et screens

--- App.js
+++ App.js
@@ -1,99 +1,48 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import AppBar from './screens/Appbar';
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import AppStack from "./screens/navigation/AppStack";
-import AppDrawer from "./screens/navigation/AppDrawer";
-import AuthProvider, { AuthContext } from "./screens/context/AuthContext";
-
-
-
-// --- Création des navigateurs ---
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-function DetailsScreen({ route }) {
+import { useEffect, useState, useContext } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { initDB } from "./services/database";
+import { ThemeProvider, ThemeContext } from "./context/ThemeContext";
+import TodoListOfflineScreen from "./screens/TodoListOfflineScreen";
+function MainApp() {
+const { theme } = useContext(ThemeContext);
 return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text>📄 Écran de détails</Text>
-{route.params && <Text>ID reçu : {route.params.id}</Text>}
-</View>
+ <View
+ style={[
+ styles.container,
+ theme === "dark" ? styles.dark : styles.light,
+ ]}
+ >
+ <TodoListOfflineScreen />
+ </View>
 );
 }
-function SettingsScreen() {
-return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text>⚙ Paramètres</Text>
-</View>
-);
-}
-// --- Navigation par pile ---
-function HomeStack() {
-return (
-<Stack.Navigator  screenOptions={{ headerShown: false }}>
-<Stack.Screen name="Accueil" component={HomeScreen} options={{
-headerStyle: { backgroundColor: '#007AFF' }, headerTintColor: '#fff', headerTitleStyle: {
-fontWeight: 'bold' },
-}}
-/>
-<Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Mes Détails Personnalisés' }}
-/>
-</Stack.Navigator>
-);
-}
-// --- Navigation par onglets ---
-function MainTabs() {
-  return (
-    <>
-      <AppBar />
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: { backgroundColor: '#f0f0f0' },
-          tabBarLabelStyle: { fontSize: 14 },
-        }}
-      >
-        <Tab.Screen
-          name="Maison"
-          component={HomeStack}
-          options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="home" size={size} color={color} />), }}
-        />
-        <Tab.Screen
-          name="Tâches"
-          component={AppStack}
-          options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="list" size={size} color={color} />), }}
-        />
-        <Tab.Screen
-          name="Paramètres"
-          component={SettingsScreen}
-          options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="settings" size={size} color={color} />), }}
-        />
-      </Tab.Navigator>
-    </>
-  );
-}
-
-function RootNavigator() {
-  const { user } = useContext(AuthContext) || {};
-  return user ? <AppDrawer /> : <LoginScreen />;
-}
-
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AuthProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </AuthProvider>
-    </Provider>
-  );
+const [dbReady, setDbReady] = useState(false);
+useEffect(() => {
+ const prepareDb = async () => {
+ await initDB(); // attendre SQLite
+ setDbReady(true); // OK pour afficher l’app
+ };
+ prepareDb();
+}, []);
+if (!dbReady) {
+ return <ActivityIndicator size="large" />;
 }
+return (
+ <ThemeProvider>
+ <MainApp />
+ </ThemeProvider>
+);
+}
+const styles = StyleSheet.create({
+container: {
+ flex: 1,
+ paddingTop: 40,
+},
+light: {
+ backgroundColor: "#ffffff",
+},
+dark: {
+ backgroundColor: "#121212",
+},
+})
